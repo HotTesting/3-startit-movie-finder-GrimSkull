3c769366be7ac68d8e8e3d59a4976b33eb1fdba0
Igor Karpov <igor_karpov@ukr.net>
2017-03-07 13:34:33 +0200

    Add postconditions & Jasmine2Reporter

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,5 +1,22 @@
 module.exports.config = {
-    specs: ['home-works/searchTests.js'],
+    specs: ['test-cases/searchTests.js'],
     directConnect: true,
-    baseUrl: 'https://movies-finder.firebaseapp.com/'
+    baseUrl: 'https://movies-finder.firebaseapp.com/',
+
+    onPrepare:() => {
+        let Jasmine2Reporter = require('jasmine2-reporter').Jasmine2Reporter
+        jasmine.getEnv().addReporter(new Jasmine2Reporter());
+    },
+
+
+    beforeEach:() => {
+        
+    },
+
+    afterEach:() => {
+        browser.get('/')
+        browser.executeScript('window.sessionStorage.clear();')
+        browser.executeScript('window.localStorage.clear();')
+        browser.manage().deleteAllCookies()
+    },
 }
