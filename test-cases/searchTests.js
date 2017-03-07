f40f9ee5ba3579f125566f6e82b84c8b6a1166f6
Igor Karpov <igor_karpov@ukr.net>
2017-03-07 13:21:43 +0200

    Add postconditions & include Jasmine2Reporter

--- test-cases/searchTests.js
+++ test-cases/searchTests.js
@@ -1,10 +1,10 @@
 describe('Search Field Tests', ()=> {
 
     let searchField = $('[name = "searchStr"]')
-    let movieNameSearch = 'Star Wars'
+    let movieNameSearch = 'star wars'
     let searchMovieByGenre = 'Horror'
     let movieYearSearch = '2015'
-    let searchMovieByActor = 'Brad Pitt'
+    let searchMovieByActor = 'brad pitt'
     let inputMisspelled = 'pirrates'
     let cyrillicInputSearch = 'маска'
     let randomInputSearch = 'ldjklj'
@@ -12,51 +12,53 @@ describe('Search Field Tests', ()=> {
     let upperCaseInputSearch = "JAWS"
     let specialSymbolsInput = '%?>@#<*+-='
     
-    it('by movie Name', ()=> {
+    xit('by movie Name', ()=> {
         browser.get('')
-                
+
         searchField.sendKeys(movieNameSearch)
         element(by.buttonText('Go!')).click()
         browser.sleep(5000)
 
-        let title = $('movie-card').$('h4 a').getText()
+        let title = $$('movie-card').first().$('h4 a').getText().then(text => text.toLowerCase())
         expect(title).toContain(movieNameSearch, 'Movie name does not found')
     })
 
-    it('by movie Year', ()=> {
+    xit('by movie Year', ()=> {
         browser.get('')
-                
+
         searchField.sendKeys(movieYearSearch)
         element(by.buttonText('Go!')).click()
         browser.sleep(5000)
 
-        let year = $('movie-card').$('div p').getText()
+        let year = $$('movie-card').first().$('div p').getText()
         expect(year).toContain(movieYearSearch, 'Does not find movie of such year')
     })
 
-     it('by movie Genre', ()=> {
+     it('by Genre', ()=> {
         browser.get('')
-                
+
         searchField.sendKeys(searchMovieByGenre)
         element(by.buttonText('Go!')).click()
         browser.sleep(5000)
 
-        element(by.linkText('View details »')).click()
+        $$('movie-card').first().element(by.linkText('View details »')).click()
         browser.sleep(2000)
         
+        let genre = $$('.col-md-8').first().$('p > a')
+
         expect(element(by.linkText(searchMovieByGenre)).getTagName()).toBe('a', 'This movie is not a horror genre')
         
     })  
 
-     it('by actor name', ()=> {
+     xit('by actor name', ()=> {
         browser.get('')
-                
+
         searchField.sendKeys(searchMovieByActor)
         element(by.buttonText('Go!')).click()
         browser.sleep(5000)
 
         // Code below need to be used in case if no one movie found:
-        let searchResult = $('div h3').getText()
+        let searchResult = $('div h3').getText().then(text => text.toLowerCase())
         expect(searchResult).toContain('Search Results', 'No matching with search')
 
         // Code below need to be used in case if some movie will found:
@@ -67,21 +69,21 @@ describe('Search Field Tests', ()=> {
          
     })
     
-    it('by input misspelled', ()=> {
+    xit('by input misspelled', ()=> {
         browser.get('')
-                
+        
         searchField.sendKeys(inputMisspelled)
         element(by.buttonText('Go!')).click()
         browser.sleep(5000)
 
-        let searchResult = $('h3').getText()
+        let searchResult = $('h3').getText().then(text => text.toLowerCase())
         expect(searchResult).toContain('Search Results', 'no matching with search')
         // How to compare empty search result with expected?
     })
 
-    it('by cyrillic input', ()=> {
+    xit('by cyrillic input', ()=> {
         browser.get('')
-                
+        
         searchField.sendKeys(cyrillicInputSearch)
         element(by.buttonText('Go!')).click()
         browser.sleep(5000)
@@ -90,7 +92,7 @@ describe('Search Field Tests', ()=> {
         expect(searchResult).toContain('The Mask', "'The Mask' movie did not find")
     })
 
-    it('by random input', ()=> {
+    xit('by random input', ()=> {
         browser.get('')
                 
         searchField.sendKeys(randomInputSearch)
@@ -110,7 +112,7 @@ describe('Search Field Tests', ()=> {
         // How to compare empty search result with expected?
     }) 
 
-    it('by empty input', ()=> {
+    xit('by empty input', ()=> {
         browser.get('')
                 
         searchField.sendKeys(emptyInput)
@@ -130,7 +132,7 @@ describe('Search Field Tests', ()=> {
         // How to compare empty search result with expected?
     })
 
-    it('by special symbols input', ()=> {
+    xit('by special symbols input', ()=> {
         browser.get('')
                 
         searchField.sendKeys(specialSymbolsInput)
@@ -150,14 +152,14 @@ describe('Search Field Tests', ()=> {
         // The same problem - how to compare empty search result with expected?
     })
 
-    it('by uppercase input', ()=> {
+    xit('by uppercase input', ()=> {
         browser.get('')
                 
         searchField.sendKeys(upperCaseInputSearch)
         element(by.buttonText('Go!')).click()
         browser.sleep(5000)
 
-        let searchResult = element(by.linkText('Jaws')).getText()
-        expect(searchResult).toContain('Jaws', 'Movie do not find')
+        let searchResult = element(by.linkText('Jaws')).getText().then(text => text.toUpperCase())
+        expect(searchResult).toContain(upperCaseInputSearch, 'Movie do not find')
     })
 })
